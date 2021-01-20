39d41e2d510b63fc3435936a1ef0543f4668b7e9
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2021-01-20 13:23:20 -0800

    have all preperation

---
+++ GeekWeatherWidgetIntentExtension/NetworkLayerIntentExtension.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkLayerIntentExtension.swift
+//  GeekWeatherWidgetIntentExtension
+//
+//  Created by Cyril Garcia on 1/20/21.
+//  Copyright © 2021 ByCyril. All rights reserved.
+//
+
+import Foundation

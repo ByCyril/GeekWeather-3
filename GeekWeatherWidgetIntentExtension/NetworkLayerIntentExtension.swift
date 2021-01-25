fb15a40b429fb109c2e7eaf43af8bbbd85a1c055
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2021-01-24 16:40:58 -0800

    fixed issue

--- GeekWeatherWidgetIntentExtension/NetworkLayerIntentExtension.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NetworkLayerIntentExtension.swift
-//  GeekWeatherWidgetIntentExtension
-//
-//  Created by Cyril Garcia on 1/20/21.
-//  Copyright © 2021 ByCyril. All rights reserved.
-//
-
-import Foundation
